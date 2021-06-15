29a9a3713432e8f5f7efbdd86bebbbd41a6e0403
Sahil Patel <sp0715007@gmail.com>
2021-06-15 18:46:34 -0400

    router set up and firebase setup

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -21,7 +21,7 @@ const Register = () => {
   );
 
   return (
-    <div className="contaner p-5">
+    <div className="container p-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <h4>Register</h4>
