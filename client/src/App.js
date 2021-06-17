3a3ed32edfa9971b06d5e59eaaa1e1732125cf99
Sahil Patel <sp0715007@gmail.com>
2021-06-17 17:46:07 -0400

    user stored in redux state

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -10,8 +10,36 @@ import Header from "./component/nav/Header";
 import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import RegisterComplete from "./pages/auth/RegisterComplete";
+import { auth } from './firebase'
+import { useDispatch } from "react-redux";
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  // to check firebase auth state
+
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged( async (user) => {
+      if(user){
+        //getting a user Token
+        const idTokenResult = await user.getIdTokenResult();
+        console.log('user', user);
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token, 
+          }
+        })
+      }
+    })
+
+    //clean up and sipatch to redux
+    return () => unsubscribe()
+
+  }, [])
+
   return (
     <>
       <Header />
