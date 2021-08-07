5cfe5cba3032d03b4ad1c15791f4fae7ed0a44e7
Sahil Patel <sp0715007@gmail.com>
2021-08-07 02:16:38 -0400

    Star rating done with backend and frontend

--- client/src/component/modal/RatingModal.js
+++ client/src/component/modal/RatingModal.js
@@ -3,14 +3,29 @@ import { Modal, Button } from "antd";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { StarOutlined } from "@ant-design/icons";
+import { useHistory, useParams } from "react-router-dom";
 
 const RatingModal = ({ children }) => {
   const { user } = useSelector((state) => ({ ...state }));
   const [modalVisible, setModalVisible] = useState(false);
 
+  let history = useHistory();
+  let { slug } = useParams();
+
+  const handleModal = () => {
+    if (user && user.token) {
+      setModalVisible(true);
+    } else {
+      history.push({
+        pathname: "/login",
+        state: { from: `/product/${slug}` },
+      });
+    }
+  };
+
   return (
     <>
-      <div onClick={() => setModalVisible(true)}>
+      <div onClick={handleModal}>
         <StarOutlined className="text-danger" /> <br />{" "}
         {user ? "Leave rating" : "Login to leave rating"}
       </div>
