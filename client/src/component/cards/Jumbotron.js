81d97544c8b4b0192a814188b2148d0707c1c8e2
Sahil Patel <sp0715007@gmail.com>
2021-11-12 20:19:28 -0500

    ENV

--- client/src/component/cards/Jumbotron.js
+++ client/src/component/cards/Jumbotron.js
@@ -2,6 +2,7 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 const Jumbotron = ({ text }) => (
+
   <Typewriter
     options={{
       strings: text,
--- client/src/functions/auth.js
+++ client/src/functions/auth.js
@@ -13,6 +13,7 @@ export const createOrUpdateUser = async (authtoken) => {
     );
   };
 
+  
   export const currentUser = async (authtoken) => {
     return await axios.post(
       `${process.env.REACT_APP_API}/current-user`,
